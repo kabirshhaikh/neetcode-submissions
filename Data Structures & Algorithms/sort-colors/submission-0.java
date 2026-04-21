ef4fb22f5771222d0513ed6813abb24c5a136ff3
Kabir Shaikh <114681108+kabirshhaikh@users.noreply.github.com>
2026-04-20 23:34:10 -0400

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int zeroes = 0;
+        int ones = 0;
+        int twos = 0;
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zeroes++;
+            } else if (nums[i] == 1) {
+                ones++;
+            } else {
+                twos++;
+            }
+        }
+
+        for (int i = 0; i < zeroes; i++) {
+            nums[index] = 0;
+            index++;
+        }
+
+        for (int i = 0; i < ones; i++) {
+            nums[index] = 1;
+            index++;
+        }
+
+        for (int i = 0; i < twos; i++) {
+            nums[index] = 2;
+            index++;
+        }
+    }
+}
