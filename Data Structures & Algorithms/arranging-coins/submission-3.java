0b11dff746630f6285ef106e3adc077a4c84dee2
Kabir Shaikh <114681108+kabirshhaikh@users.noreply.github.com>
2026-06-21 21:39:30 -0400

    Add: arranging-coins - submission-3

---
+++ Data Structures & Algorithms/arranging-coins/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        if (n < 1) {
+            return -1;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        int output = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n - i > 0) {
+                output++;
+                n = n - i;
+            } else {
+                break;
+            }
+        }
+
+        return output;
+    }
+}
